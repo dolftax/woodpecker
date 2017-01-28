a921c9bb84391d9dcbe02b3b14c7cefeda04cf7f
Jaipradeesh <jaipradeesh@gmail.com>
2017-01-29 02:13:40 +0530

    Add monitor function and sample config values
    - Check RSS of all workers every 5000ms
    - If heap is increased more than defined size
      - Disconnect the worker
      - Force kill the worker after 4000ms
      - Spawn a new fork
    
    Signed-off-by: Jaipradeesh <jaipradeesh@gmail.com>

--- example.js
+++ example.js
@@ -1,5 +1,6 @@
 let woodpecker = require('./woodpecker')
 let http = require('http')
+let cluster = require('cluster')
 
 // Woodpecker config
 let config = {
@@ -7,15 +8,18 @@ let config = {
     port: 8080
   },
   woodpecker: {
+    // 3 GB / 10 (Number of workers + master) in bytes
+    maxRssSize: 322122547,
     refreshInterval: 1000
   }
 }
 
 let entry = function () {
   http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'})
-    response.end('Tuk tuk\n')
+    response.writeHead(200, { 'Content-Type': 'application/json' })
+    response.write(JSON.stringify({message: `I\'m from worker ${cluster.worker.id}`}))
+    response.end()
   }).listen(config.app.port)
 }
 
-woodpecker.init(entry)
+woodpecker.init(entry, config.woodpecker)
--- woodpecker.js
+++ woodpecker.js
@@ -1,30 +1,62 @@
 const cluster = require('cluster')
 const numCPUs = require('os').cpus().length
+const execSync = require('child_process').execSync
 
 let woodpecker = {}
 
 // `entry` should be a fuction which will be executed across
 // master and workers
 woodpecker.init = (entry, config) => {
-  // If process is master, fork (2 x numCPUs) + 1 workers
   if (cluster.isMaster) {
-    for (let i = 0; i < ((2 * numCPUs) + 1); i++) {
+    // If process is master, fork (2 x numCPUs) + 1 workers
+    let numWorkers = (2 * numCPUs) + 1
+    for (let i = 0; i < numWorkers; i++) {
       cluster.fork()
     }
+    console.log(`${numWorkers} workers forked/spawned`)
+
+    // Monitor the workers every 5000 ms
+    setInterval(() => {
+      for (const id in cluster.workers) {
+        woodpecker.monitor(cluster.workers[id], config.maxRssSize)
+      }
+    }, 5000)
   } else {
     // Invoke entry function on all workers
     entry()
   }
+}
 
-  setInterval(() => {
-    woodpecker.monitor()
-  }, 5000)
+// Fork a new worker
+woodpecker.fork = () => {
+  console.log('Fork/Spawn -ing a new worker')
+  cluster.fork()
 }
 
 // Monitor function watches for RSS size
-woodpecker.monitor = () => {
-  if (!cluster.Master) {
-    console.log(cluster.worker)
+woodpecker.monitor = (worker, maxRssSize) => {
+  let timeout
+  // There is no direct implementation to get memoryUsage of a worker process
+  // Read more here - https://github.com/nodejs/help/issues/469
+  let currentRss = execSync(`awk '/Rss:/{ sum += $2 } END { print sum }' /proc/${worker.process.pid}/smaps`)
+
+  // Check if process RSS is more than defined max value (in bytes)
+  if (currentRss >= maxRssSize) {
+    // Gracefully disconnect the worker from master
+    worker.disconnect(woodpecker.fork)
+
+    // Force kill the worker after 4000 ms if not disconnected
+    timeout = setTimeout(() => {
+      worker.kill()
+      woodpecker.fork()
+      console.log(`Worker {worker.id} killed | RSS {currentRss}`)
+    }, 4000)
+
+    // If worker disconnects graccefully, clear the `force kill` timeout
+    worker.on('disconnect', () => {
+      console.log(`Worker {worker.id} disconnected | RSS {currentRss}`)
+      clearTimeout(timeout)
+    })
   }
 }
 
