5cd6e37a73fcd13f8c09c71d965addc1d254f8ff
Jaipradeesh <jaipradeesh@gmail.com>
2017-01-30 22:49:29 +0530

    Reduce memory allocation to 55150 bytes per request
    Signed-off-by: Jaipradeesh <jaipradeesh@gmail.com>

--- example.js
+++ example.js
@@ -8,19 +8,17 @@ let config = {
     port: 8080
   },
   woodpecker: {
-    // 3 GB / 100 (Number of workers + master) in bytes
-    maxRssSize: 32212254,
+    maxRssSize: 80000,
     refreshInterval: 5000
   }
 }
 
 let entry = function () {
   http.createServer(function (request, response) {
-    // Allocate memory x per request which is reachable by GC
+    // Allocate 55150 bytes per request (Reachable by GC)
     let memAlloc = []
-    for (let i = 0; i < 6e6; i++) {
+    for (let i = 0; i < 6e3; i++) {
       memAlloc.push(new Buffer(1))
-      new Buffer(Buffer.poolSize - 2)
     }
 
     response.writeHead(200, { 'Content-Type': 'application/json' })
